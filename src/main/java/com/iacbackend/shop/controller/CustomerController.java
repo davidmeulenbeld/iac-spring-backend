0fa05432ad8f3e7373c1af1a7c03c6e95f1996bc
David Meulenbeld <davidmeulenbeld@Davids-MacBook-Pro.local>
2020-04-04 11:39:41 +0200

    [FEATURE] Start Spring Webshop

---
+++ src/main/java/com/iacbackend/shop/controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.iacbackend.shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CustomerController {
+
+    @RequestMapping("/")
+    public String getCustomer() {
+        return "joeeeee";
+    }
+}
